6e04773a5bf21115a10c8172485104a611fdab39
Aryan kumar <aryhu@Aryans-MacBook-Air.local>
2025-01-20 07:52:44 +0530

    improve socket connection

--- src/index.ts
+++ src/index.ts
@@ -57,6 +57,9 @@ io.on("connection", (socket) => {
     console.log("User Joined Room: " + room);
   });
 
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
   socket.on("new message", (newMessage) => {
     const chat = newMessage.chat;
     if (!chat.users) return console.error("Chat users not defined");
